0f0c58f406aa73e037f49b9492af7f901efe7ec8
Umberto Griffo <1609440+umbertogriffo@users.noreply.github.com>
2023-12-27 16:59:34 +0000

    fix: log writing multiple times

--- chat/helpers/log.py
+++ chat/helpers/log.py
@@ -6,13 +6,12 @@ import sys
 def get_logger(name: str):
     level = os.environ.get("LOGLEVEL", "INFO").upper()
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    if not logger.hasHandlers():
+        logger.setLevel(level)
 
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(level)
-    formatter = logging.Formatter(
-        "[%(thread)d] %(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        formatter = logging.Formatter("[%(thread)d] %(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
---
+++ demo.md
@@ -0,0 +1,13 @@
+# Story Chatbot - 1
+
+- Tell me something about Italy
+- How many people live there?
+- How can I get a Golden visa?
+- Could you please remind me about the topic we were discussing earlier?
+
+# Story Chatbot - 2
+
+- In which country is Italy?
+- Can you tell me the names of the countries that share a border with Italy?
+- Could you please provide me with information on the main industries?
+- Could you please remind me about the topic we were discussing earlier?
