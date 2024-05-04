6f734ad534bb83465924a7aa9f73ab85e307c096
Umberto Griffo <1609440+umbertogriffo@users.noreply.github.com>
2024-05-04 14:24:11 +0100

    feat: added tests for the lamacpp client

--- tests/test_lamacpp_client.py
+++ tests/test_lamacpp_client.py
@@ -59,6 +59,15 @@ def test_start_answer_iterator_streamer(lamacpp_client):
     assert "rome" in generated_answer.lower()
 
 
+def test_parse_token(lamacpp_client):
+    prompt = "What is the capital city of Italy?"
+    stream = lamacpp_client.start_answer_iterator_streamer(prompt, max_new_tokens=10)
+    generated_answer = ""
+    for output in stream:
+        generated_answer += lamacpp_client.parse_token(output)
+    assert "rome" in generated_answer.lower()
+
+
 @pytest.mark.asyncio
 async def test_async_generate_answer(lamacpp_client):
     prompt = "What is the capital city of Italy?"
@@ -75,13 +84,4 @@ async def test_async_start_answer_iterator_streamer(lamacpp_client):
     generated_answer = ""
     for output in stream[0]:
         generated_answer += output["choices"][0]["text"]
-    assert "rome" in generated_answer.lower()
-
-
-def test_parse_token(lamacpp_client):
-    prompt = "What is the capital city of Italy?"
-    stream = lamacpp_client.start_answer_iterator_streamer(prompt, max_new_tokens=10)
-    generated_answer = ""
-    for output in stream:
-        generated_answer += lamacpp_client.parse_token(output)
     assert "rome" in generated_answer.lower()
