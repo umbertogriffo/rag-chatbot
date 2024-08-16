4d5a733cfcffd7ca7c564007f69c97fca4059c8d
Umberto Griffo <1609440+umbertogriffo@users.noreply.github.com>
2024-08-16 08:27:49 +0100

    chore: bump lama-cpp-python to 0.2.79

--- chatbot/helpers/log.py
+++ chatbot/helpers/log.py
@@ -6,9 +6,11 @@ import sys
 def get_logger(name: str):
     level = os.environ.get("LOGLEVEL", "INFO").upper()
     logger = logging.getLogger(name)
+
+    # Prevent double configuration by checking handlers
     if not logger.hasHandlers():
         logger.setLevel(level)
-
+        # Stream handler to stdout
         handler = logging.StreamHandler(sys.stdout)
         handler.setLevel(level)
         formatter = logging.Formatter("[%(thread)d] %(asctime)s - %(name)s - %(levelname)s - %(message)s")
